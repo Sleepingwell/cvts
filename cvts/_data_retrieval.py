9f97f039940b6ab82e186d70cad4102d5fc6fedd
Pedro Camargo <c@margo.co>
2021-09-01 14:25:17 +1000

    Incorporates changes for raw data in one file per vehicle/month

--- cvts/_data_retrieval.py
+++ cvts/_data_retrieval.py
@@ -4,9 +4,22 @@ from datetime import date
 from typing import Iterable
 import pandas as pd
 from .settings import RAW_PATH
-
+from sqlalchemy import create_engine
 
 class NoRawDataException(Exception): pass
+
+datalake_connection_string = "postgresql://cvts:csiro_and_world_bank@10.100.0.50:5432/datalake"
+
+def list_of_vehicles() -> pd.DataFrame:
+    """Queries the infrastructure for a table with all vehicles for which we have data to analyze
+
+    :return: A Pandas DataFrame with all vehicles we have in the database
+    """
+    engine = create_engine(datalake_connection_string)
+    conn = engine.connect()
+
+    sql = 'select * from vehicles veh join vehicle_types vt on (veh.vehicle_type_id=vt.id)'
+    return pd.read_sql(sql, conn)
 
 
 def vehicle_trace(vehicle_id: str, dates: Iterable[date]) -> dict:
